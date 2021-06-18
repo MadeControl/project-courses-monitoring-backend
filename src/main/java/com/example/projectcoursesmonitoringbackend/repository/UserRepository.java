a3b5497f28cbecb678c6e8d78766a9774f80607b
MadeControl <danya.polishchuk.2001@ukr.net>
2021-06-18 14:26:14 +0300

    added validation of not existing user

--- src/main/java/com/example/projectcoursesmonitoringbackend/repository/UserRepository.java
+++ src/main/java/com/example/projectcoursesmonitoringbackend/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.example.projectcoursesmonitoringbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByEmail(String email);
+    Optional<User> findUserByEmail(String email);
 
     boolean existsUserByEmail(String email);
 
--- src/main/java/com/example/projectcoursesmonitoringbackend/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/projectcoursesmonitoringbackend/security/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.projectcoursesmonitoringbackend.security;
 
+import com.example.projectcoursesmonitoringbackend.exception.NotFoundException;
 import com.example.projectcoursesmonitoringbackend.model.User;
 import com.example.projectcoursesmonitoringbackend.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private static final String USER_NOT_FOUND_EXCEPTION_MESSAGE = "User was not found by Email ";
 
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -18,7 +20,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findUserByEmail(email);
+        User user = userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_EXCEPTION_MESSAGE + email));
         return new UserDetailsImpl(user);
     }
 }
